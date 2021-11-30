956fa40783f7bd23ccd9aae21a37280d82f5779e
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-11-30 02:26:57 +0100

    Create a table named animals and input all column headers

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempt INT,
+    neutered BOOLEAN,
+    WEIGHT_KG DECIMAL,
+    PRIMARY KEY(id)
 );
