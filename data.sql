57df3ff225680708d9df92e1ee2eec4f9f13a228
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-12-01 09:43:33 +0100

    Insert new values of different pokemons to the animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
+
 INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, WEIGHT_KG) 
 VALUES ('Agumon', '2020-02-03', 0, true, 10.23);
 INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, WEIGHT_KG) 
@@ -8,3 +9,8 @@ INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, WEIGHT_KG)
 VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, WEIGHT_KG) 
 VALUES ('Devimon', '2017-05-12', 5, true, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, WEIGHT_KG) 
+VALUES ('Charmander', '2020-02-08', 0, false, -11), ('Plantmon', '2022-11-15', 2, true, -5.7),
+('Squirtle', '1993-04-02', 3, false, -12.13), ('Angemon', '2005-06-12', 1, true, -45),
+ ('Boarmon', '2005-06-07', 7, true, 20.4),
+('Blossom', '1998-10-13', 3, true, 17);
