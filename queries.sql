270987d3351c72da792abd527f690768b3e86c84
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-12-02 09:16:45 +0100

    Add queries that uses join and aggregate functions to the queries.sql file

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-/*Queries that provide answers to the questions from all projects.*/
+-- /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
 
@@ -30,3 +30,47 @@ SELECT species, MIN(WEIGHT_KG), MAX(WEIGHT_KG) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempt) FROM animals 
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
+
+SELECT owners.id, full_name, name
+FROM owners
+JOIN animals
+ON owners.id = owner_id
+WHERE full_name = 'Melody Pond';
+
+SELECT species.id, species.name, animals.name
+FROM species 
+JOIN animals
+ON species.id = species_id
+WHERE species.name = 'Pokemon'; 
+
+SELECT owners.id, owners.full_name, animals.name
+FROM owners
+LEFT JOIN animals
+ON owners.id = owner_id;
+
+SELECT species.id, species.name, COUNT(animals.name)
+FROM species 
+JOIN animals
+ON species.id = species_id
+GROUP BY species.id;
+
+SELECT species_id, species.name AS specie, animals.name
+FROM species 
+JOIN animals
+ON species.id = species_id
+JOIN owners
+ON owners.id = owner_id
+WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+
+SELECT owners.full_name, animals.name
+FROM owners
+JOIN animals
+ON owners.id = owner_id
+WHERE full_name = 'Dean Winchester' AND escape_attempt = 0; 
+
+SELECT owners.full_name, COUNT(owners.id)
+FROM owners
+JOIN animals
+ON owners.id = owner_id
+GROUP BY owners.id
+ORDER BY COUNT(owners.id) DESC;
