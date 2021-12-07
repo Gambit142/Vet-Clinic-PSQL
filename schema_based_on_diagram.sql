77b6cd062d92cac0196c85248709f10eb9b9c464
Busiwa24 <busiwaliuma@gmail.com>
2021-12-07 17:56:45 +0200

    Create tables according to schema diagram and add indexes on foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -59,3 +59,47 @@ CREATE TABLE invoices (
 );
 
 Create Index invoices_medical_history_id on invoices(medical_history_id);
+
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY INT,
+  type VARCHAR(200),
+  name VARCHAR(200),
+);
+
+CREATE TABLE diagnosis (
+  medical_histories_id INT,
+  treatments_id INT,
+  CONSTRAINT fk_medical_histories
+    FOREIGN KEY(medical_histories_id)
+	  REFERENCES medical_histories(id),
+  CONSTRAINT fk_treatments
+    FOREIGN KEY(treatments_id)
+	REFERENCES treatments(id)
+);
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY INT,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT
+    CONSTRAINT fk_invoice
+    FOREIGN KEY(invoice_id)
+	REFERENCES invoices(id)
+);
+
+Create Index invoice_items_invoice_id on invoice_items(invoice_id);
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY INT,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT,
+    CONSTRAINT fk_medical_history_invoice
+    FOREIGN KEY(medical_history_id)
+	REFERENCES medical_histories(id)
+);
+
+Create Index invoices_medical_history_id on invoices(medical_history_id);
